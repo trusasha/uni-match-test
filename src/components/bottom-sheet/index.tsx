1e0585d6c5a9c27c0db6aca6a62916f2d0a93911
trusasha <hts.sasha800i@gmail.com>
2024-03-27 02:09:14 +0100

    added backdrop

--- src/components/bottom-sheet/index.tsx
+++ src/components/bottom-sheet/index.tsx
@@ -2,9 +2,9 @@ import { COLORS } from 'constants/colors';
 
 import type { FC } from 'react';
 import React from 'react';
-import type { BottomSheetProps } from '@gorhom/bottom-sheet';
+import type { BottomSheetProps, BottomSheetBackdropProps } from '@gorhom/bottom-sheet';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import GorhomBottomSheet from '@gorhom/bottom-sheet';
+import GorhomBottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
 import { StyleSheet } from 'react-native';
 
 interface Props extends BottomSheetProps {}
@@ -12,12 +12,17 @@ interface Props extends BottomSheetProps {}
 export const BottomSheet: FC<Props> = ({ ...rest }) => {
   return (
     <GorhomBottomSheet
+      backdropComponent={renderBackdrop}
       backgroundStyle={styles.container}
       handleIndicatorStyle={styles.indicator}
       {...rest}
     />
   );
 };
+
+const renderBackdrop: FC<BottomSheetBackdropProps> = props => (
+  <BottomSheetBackdrop {...props} style={styles.backdrop} disappearsOnIndex={-1} appearsOnIndex={0} />
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -30,5 +35,8 @@ const styles = StyleSheet.create({
     width: 40,
     height: 4,
     borderRadius: 4,
+  },
+  backdrop: {
+    backgroundColor: 'rgba(136, 136, 136, 0.8)',
   },
 });
